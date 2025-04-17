ccbad5e5f90e567b201aefc485a4fc2ecde5795e
Minty <nyahstic@gmail.com>
2025-04-17 15:18:40 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using MythosBot.Languages;
+
+namespace MythosBot;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        new LanguageManager();
+        var langMan = LanguageManager.Instance;
+
+        Console.WriteLine($"Hello {langMan.GetToken("bot.name")}");
+    }
+}
