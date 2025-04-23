149d2cdb1fb29862042c29a90317fe340c1584f5
Minty <nyahstic@gmail.com>
2025-04-23 16:18:57 -0300

    Update Logger.cs

--- MythosBot/Logger.cs
+++ MythosBot/Logger.cs
@@ -1,4 +1,5 @@
-﻿// First Party
+// First Party
+using System.Diagnostics;
 
 // Third Party
 using Discord;
@@ -9,8 +10,12 @@ namespace MythosBot;
 
 public class Logger
 {
+    public static Stopwatch stopwatch = new Stopwatch();
     public Logger(DiscordSocketClient client, CommandService service)
     {
+        if (!stopwatch.IsRunning)
+            stopwatch.Start();
+
         if (client is not null)
             client.Log += LogMessage;
         if (service is not null)
@@ -19,23 +24,43 @@ public class Logger
 
     public async Task LogMessage(LogMessage arg)
     {
-        Console.ForegroundColor = arg.Severity == LogSeverity.Info || arg.Severity == LogSeverity.Verbose ? ConsoleColor.Cyan : ConsoleColor.Yellow;
+        switch (arg.Severity)
+        {
+            case LogSeverity.Critical:
+            case LogSeverity.Error:
+                Console.ForegroundColor = ConsoleColor.Red;
+                break;
+            case LogSeverity.Warning:
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                break;
+            case LogSeverity.Info:
+            case LogSeverity.Verbose:
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                break;
+            case LogSeverity.Debug:
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                break;
+            default:
+                break;
+        }
+
+        var ts = stopwatch.Elapsed;
+        Console.Write("[{0:00}:{1:00}:{2:00}.{3:000} -", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds);
+
         if (arg.Exception is CommandException cmdExc)
         {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine($"[Comandos/Exceção] Um erro aconteceu enquanto tentava executar o comando {cmdExc.Command}.");
+            Console.WriteLine($"- Comandos/Exceção] Um erro aconteceu enquanto tentava executar o comando {cmdExc.Command}.");
             
             Console.Error.WriteLine(cmdExc);
         }
         else if (arg.Exception is not null)
         {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine($"[Exceção] {arg.Exception.Message}");
+            Console.WriteLine($"- Exceção] {arg.Exception.Message}");
             Console.WriteLine(arg.Exception);
         }
         else
         {
-            Console.WriteLine($"[Genérico/{arg.Severity}] {arg.Message}");
+            Console.WriteLine($"- Genérico/{arg.Severity}] {arg.Message}");
         }
         Console.ResetColor();
         await Task.Delay(0);
